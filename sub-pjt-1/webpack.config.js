53e39d08220498d8a362c1a62bac06a83f7fdd09
onys <thkim2017@gmail.com>
2022-01-11 16:59:00 +0900

    Apply jsx and react
    entry point를 jsx도 찾을 수 있게 수정했습니다.

--- sub-pjt-1/webpack.config.js
+++ sub-pjt-1/webpack.config.js
@@ -1,5 +1,16 @@
-// const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-// module.exports = {
-//     context: path.resolve(__dirname, 'app'),
-// }
+module.exports = {
+  entry: /\.index.js?x/,
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+}
